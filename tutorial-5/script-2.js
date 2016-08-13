4c48cdb7c568b10144d69fda61a409336002ff0c
boleixiongdi <bl.jiwenlei@gmail.com>
2016-08-13 22:02:25 +0800

    modify

---
+++ tutorial-5/script-2.js
@@ -0,0 +1,31 @@
+var mainApp = angular.module("mainApp", []);
+         
+mainApp.config(function($provide) {
+   $provide.provider('MathService', function() {
+      this.$get = function() {
+         var factory = {};
+         
+         factory.multiply = function(a, b) {
+            return a * b;
+         }
+         return factory;
+      };
+   });
+});
+
+mainApp.value("defaultInput", 5);
+
+mainApp.service('CalcService', function(MathService){
+   this.square = function(a) {
+      return MathService.multiply(a,a);
+   }
+});
+
+mainApp.controller('CalcController', function($scope, CalcService, defaultInput) {
+   $scope.number = defaultInput;
+   $scope.result = CalcService.square($scope.number);
+
+   $scope.square = function() {
+      $scope.result = CalcService.square($scope.number);
+   }
+});
