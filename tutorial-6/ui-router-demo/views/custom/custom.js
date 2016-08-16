84e453aaad13d9956cd71f30013d162e9609dc3c
boleixiongdi <bl.jiwenlei@gmail.com>
2016-08-17 07:43:38 +0800

    modify angular router

--- tutorial-6/ui-router-demo/views/custom/custom.js
+++ tutorial-6/ui-router-demo/views/custom/custom.js
@@ -1,5 +1,7 @@
 angular.module('uiRouterDemoApp')
     .controller('CustomController', function ($state) {
+    	console.log($state);
+    	console.log($state.current.data);
         this.currentData = $state.current.data;
         this.state = $state;
     });
--- tutorial-6/ui-router-demo/views/params/params.js
+++ tutorial-6/ui-router-demo/views/params/params.js
@@ -1,5 +1,7 @@
 angular.module('uiRouterDemoApp')
     .controller('ParamsController', function ($scope, $stateParams, $state) {
+    	console.log($stateParams);
+    	console.log($state);
         $scope.parameters = $stateParams;
         $scope.state = $state;
     });
